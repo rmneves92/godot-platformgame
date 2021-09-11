6317f06171f18479df9e1a7246fd6c0b56d8d906
Rafael Neves <rafa.mneves@hotmail.com>
2021-09-11 14:36:29 -0300

    add particles

--- scripts/start.gd
+++ scripts/start.gd
@@ -11,4 +11,4 @@ func _input(event):
 			yield($texto/anim_texto, "animation_finished")
 			$color/anim_color.play("animar_color")
 			yield($color/anim_color, "animation_finished")
-			print("mudar de fase")
+			get_tree().change_scene("res://scenes/fase01.tscn")
