fa7636904c22d670d782abd9a19323e9d2b160aa
Rafael Neves <rafa.mneves@hotmail.com>
2021-09-15 08:10:19 -0300

    improve platform logic

--- scripts/plataforma.gd
+++ scripts/plataforma.gd
@@ -5,24 +5,41 @@ var pos_final = Vector2()
 var distancia = 300
 var vel = 100
 var dir = 1
-
+export var lado = 1
 
 func _ready():
-	pos_inicial = position
-	pos_final = pos_inicial + Vector2(distancia * dir, 0)
+	if lado == 1:
+		pos_inicial = position
+		pos_final = pos_inicial + Vector2(distancia * dir, 0)
+	if lado == -1:
+		pos_inicial = position
+		pos_final = pos_inicial - Vector2(distancia * dir, 0)
 
 func _physics_process(delta):
-	if dir == 1:
-		vel = 100
-		position.x += vel * delta
-		if position.x >= pos_final.x:
-			dir *= -1
-	
-	if dir == -1:
-		vel = -100
-		position.x += vel * delta
-		if position.x <= pos_inicial.x:
-			dir *= -1
+	if lado == 1:
+		if dir == 1:
+			vel = 100
+			position.x += vel * delta
+			if position.x >= pos_final.x:
+				dir *= -1
+		
+		if dir == -1:
+			vel = -100
+			position.x += vel * delta
+			if position.x <= pos_inicial.x:
+				dir *= -1
+	else:
+		if dir == -1:
+			vel = -100
+			position.x += vel * delta
+			if position.x <= pos_final.x:
+				dir *= -1
+		
+		if dir == 1:
+			vel = 100
+			position.x += vel * delta
+			if position.x >= pos_inicial.x:
+				dir *= -1
 		
 			
 		
